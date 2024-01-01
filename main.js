c1f6048d448b5e89725be70c0dfc6f9dd325bf7d
makzuu <mr.greenmousth@gmail.com>
2024-01-01 16:30:28 -0300

    Create a 16x16 grid of square divs

---
+++ main.js
@@ -0,0 +1,21 @@
+createGrid(16, 16);
+
+function createGrid(cols, rows) {
+    const container = document.querySelector("#container");
+
+    for (let i = 0; i < rows; i++) {
+        const row = document.createElement("div");
+
+        row.classList.add("row");
+
+        for (let j = 0; j < cols; j++) {
+            const square = document.createElement("div");
+
+            square.classList.add("square");
+
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
+    }
+}
